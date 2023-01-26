Computomics/MethylScore

commit 1a2e962eb81d150ecc2ca8e78f0b54856c31111d

always sort bam files

modules/process/sort_bam_samtools.nf
@@ -6,10 +6,10 @@ process SORT {
     tuple val(sampleID), path(bam)
 
     output:
-    tuple val(sampleID), path("${sampleID}.sorted.bam"), emit: bam
+    tuple val(sampleID), path("*.sorted.bam"), emit: bam
 
     script:
     """
-    samtools sort -@ ${task.cpus} -o ${sampleID}.sorted.bam $bam
+    samtools view @ ${task.cpus} -bh -F 0x200 -F 0x4 | samtools sort -@ ${task.cpus} -o ${bam.baseName}.sorted.bam $bam
     """
-}  
+} 

modules/process/MergeSamFiles_picard.nf
@@ -1,5 +1,5 @@
 process MERGE {
-    tag "${sampleID}:${bam.name}"
+    tag "${sampleID}:${bam}"
     publishDir "${params.PROJECT_FOLDER}/01mappings/${sampleID}", mode: 'copy'
 
     input:
@@ -18,6 +18,7 @@ process MERGE {
         -Xmx${mem}m \\
         -Xms${mem / 4}m \\
         MergeSamFiles \\
+            ASSUME_SORTED=true \\
             I=${bam.join(' I=')} \\
             O=${sampleID}.merged.bam
     """

modules/process/build_genome_matrix.nf
@@ -10,15 +10,15 @@ process BUILD {
     path("*.genome_matrix.tsv"), emit: matrix
 
     script:
-    def input_format = params.METHYLPY ? "methylpy" : "bismark" 
+    def input_format = params.METHYLPY ? "methylpy" : "bismark"
     """
     sort --parallel=${task.cpus} -m -k3,3g -T . ${consensus} > ${chromosomeID}.allC
 
     perl ${projectDir}/bin/generate_genome_matrix.pl \\
-     -s ${samplesheet} \\
-     -f ${chromosomeID}.allC \\
-     -i ${input_format} \\
-     -r ${chromosomeID}.fa \\
-     -o ${chromosomeID}.genome_matrix.tsv
+        -s ${samplesheet} \\
+        -f ${chromosomeID}.allC \\
+        -i ${input_format} \\
+        -r ${chromosomeID}.fa \\
+        -o ${chromosomeID}.genome_matrix.tsv
     """
 }

modules/process/methyldackel.nf
@@ -1,4 +1,4 @@
-process EXTRACT {
+process METHYLDACKEL {
     tag "$sampleID:$chromosomeID"
     publishDir "${params.PROJECT_FOLDER}/02consensus/${sampleID}/${chromosomeID}", mode: 'copy'
 
@@ -22,6 +22,5 @@ process EXTRACT {
     ${bamsplit}
 
     sort -k2,2n <(tail -n+2 -q *bedGraph) | awk '{print "${sampleID}\\t"\$0}' > ${sampleID}.allC
-    #awk '{print "${sampleID}\\t"\$0}' <(tail -n+2 -q *bedGraph) > ${sampleID}.allC
     """
 }

modules/process/split_bam_by_chromosome.nf
@@ -7,7 +7,7 @@ process SPLIT {
     each path(fasta)
 
     output:
-    tuple val(chromosomeID), val(sampleID), path("${sampleID}.${chromosomeID}.split.bam"), emit: bam
+    tuple val(chromosomeID), val(sampleID), path("${chromosomeID}.bam"), emit: bam
     tuple val(chromosomeID), path(fasta), emit: fasta
 
     script:
@@ -16,6 +16,6 @@ process SPLIT {
     samtools index ${bam}
     cat <(samtools view -H ${bam} | grep -E "@HD|SN:${chromosomeID}\$(printf '\\t')") \\
         <(samtools view ${bam} ${chromosomeID}) \\
-        | samtools view -bo ${sampleID}.${chromosomeID}.split.bam -
+        | samtools view -bo ${chromosomeID}.bam -
     """
 }

modules/workflow/from_alignments.nf
@@ -4,8 +4,9 @@ include { MERGE       } from '../process/MergeSamFiles_picard'
 include { DEDUP       } from '../process/MarkDuplicates_picard'
 include { STATISTICS  } from '../process/get_read_statistics'
 include { SPLIT       } from '../process/split_bam_by_chromosome'
-include { EXTRACT     } from '../process/methyldackel_extract'
 include { MATRIX      } from './sub/get_genome_matrix'
+
+include { METHYLDACKEL } from '../process/methyldackel'
 
 workflow BAM {
     main:
@@ -14,17 +15,19 @@ workflow BAM {
 
     GET_SAMPLES()
 
-    GET_SAMPLES.out.samples
+    SORT(GET_SAMPLES.out.samples)
+
+    SORT.out.bam
+        .groupTuple(by:0)
         .branch { sampleID, bam ->
             multi_rep: bam.toList().size() > 1
             single_rep: true
         }
         .set { samples }
 
-    samples.single_rep | SORT
     samples.multi_rep | MERGE
 
-    alignments = SORT.out.bam.mix(MERGE.out.bam)
+    alignments = samples.single_rep.mix(MERGE.out.bam)
 
     if ( params.DO_DEDUP ) { alignments = DEDUP(alignments).bam }
 
@@ -32,10 +35,10 @@ workflow BAM {
 
     SPLIT(alignments, GET_SAMPLES.out.fasta)
 
-    EXTRACT(SPLIT.out.bam.join(SPLIT.out.fasta))
+    METHYLDACKEL(SPLIT.out.bam.join(SPLIT.out.fasta))
 
     MATRIX(
-        EXTRACT.out.consensus,
+        METHYLDACKEL.out.consensus,
         SPLIT.out.fasta,
         alignments
     )

modules/workflow/from_bedgraph.nf
@@ -8,15 +8,19 @@ workflow BEDGRAPH {
 
     GET_SAMPLES()
 
+    GET_SAMPLES.out.samples
+        .groupTuple(by:0)
+        .set { samples }
+
     SPLIT(
-        GET_SAMPLES.out.samples,
+        samples,
         GET_SAMPLES.out.fasta
     )
 
     MATRIX(
         SPLIT.out.consensus,
         SPLIT.out.fasta,
-        GET_SAMPLES.out.samples
+        samples
     )
 
     emit:

modules/workflow/sub/get_samples.nf
@@ -18,10 +18,8 @@ workflow GET_SAMPLES {
             }
         .set { inputMap }
 
-    sampleIndex = 1
     inputMap
         .map { record -> [ record.sampleID, record.filePath ] }
-        .groupTuple(by:0)
         .set {samples}
 
     emit:

commit 9fa7edc9bcd80b061a901532b7b058cfb4d63be9

Fix for SORT_BAM step failing
Moved the $bam argument to samtools view, which otherwise gets no input data and causes the step to fail. Seems like the bug was introduced in commit 1a2e962, when always sorting bam files was added.
This fixes the issue.

modules/process/sort_bam_samtools.nf
@@ -11,6 +11,6 @@ process SORT_BAM {
 
     script:
     """
-    samtools view -@ ${task.cpus} -bh -F 0x200 -F 0x4 | samtools sort -@ ${task.cpus} -o ${bam.baseName}.sorted.bam $bam
+    samtools view -@ ${task.cpus} -bh -F 0x200 -F 0x4 $bam | samtools sort -@ ${task.cpus} -o ${bam.baseName}.sorted.bam
     """
 }